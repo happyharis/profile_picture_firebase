90bfc320c1c28be069873c04a9556f984f824388
Haris <muhamad_haris@hotmail.com>
2020-09-17 19:21:30 +0800

    Add answer updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase/firebase.dart' as fb;
-import 'dart:html';
-
 import 'package:profile_picture_firebase/user.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase/firebase.dart' as fb;
+import 'dart:html';
 
 void main() {
   runApp(MyApp());
@@ -41,7 +40,7 @@ class ProfilePage extends StatelessWidget {
             SizedBox(height: 20),
             OutlineButton(
               onPressed: () {
-                buildUploadImage(_currentUser);
+                uploadToStorage(_currentUser);
               },
               child: Text('Update Profile Picture'),
             )
@@ -57,96 +56,31 @@ class ProfilePicture extends StatelessWidget {
     Key key,
   }) : super(key: key);
 
-  Future<Uri> buildDownloadURL(CurrentUser user) {
-    return fb
-        .storage()
-        .refFromURL('gs:')
-        .child(user.profileUrl)
-        .getDownloadURL();
-  }
-
   @override
   Widget build(BuildContext context) {
     final _currentUser = Provider.of<CurrentUser>(context);
     if (_currentUser == null) return Center(child: CircularProgressIndicator());
 
-    return SizedBox(
-      height: 96,
-      width: 96,
-      child: StreamBuilder<Uri>(
-        stream: Stream.fromFuture(buildDownloadURL(_currentUser)),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          }
+    return StreamBuilder<Uri>(
+      stream: downloadUrl(_currentUser.photoUrl).asStream(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        }
 
-          if (snapshot?.data == null)
-            return CircleAvatar(
-              child: Icon(
-                Icons.people_alt_rounded,
-                size: 96,
-              ),
-            );
-          return CircleAvatar(
-            backgroundColor: Colors.white,
-            child: Image.network(
-              snapshot?.data?.toString(),
-              height: 96,
-              width: 96,
-            ),
-          );
-        },
-      ),
+        return CircleAvatar(
+          radius: 48,
+          child: Image.network(snapshot.data.toString()),
+        );
+      },
     );
+    // return CircleAvatar(
+    //   radius: 48,
+    //   child: Icon(
+    //     Icons.people_alt_rounded,
+    //   ),
+    // );
   }
-}
-
-buildUploadImage(CurrentUser user) {
-  uploadImage(onSelected: (file) {
-    final userId = user.id;
-    final dateTime = DateTime.now();
-    final path = '$userId/$dateTime';
-
-    fb.storage().refFromURL('gs:').child(path).put(file).future.then((_) {
-      FirebaseFirestore.instance
-          .collection('users')
-          .doc(user.id)
-          .update({'profile_url': path});
-    });
-  });
-}
-
-/// A "select file/folder" window will appear. User will have to choose a file.
-/// This file will be then read, and uploaded to firebase storage;
-uploadImage({@required Function(File) onSelected}) async {
-  // HTML input element
-  InputElement uploadInput = FileUploadInputElement()..accept = 'image/*';
-  uploadInput.click();
-
-  uploadInput.onChange.listen(
-    (changeEvent) {
-      final file = uploadInput.files.first;
-      final reader = FileReader();
-      // The FileReader object lets web applications asynchronously read the
-      // contents of files (or raw data buffers) stored on the user's computer,
-      // using File or Blob objects to specify the file or data to read.
-      // Source: https://developer.mozilla.org/en-US/docs/Web/API/FileReader
-
-      reader.readAsDataUrl(file);
-      // The readAsDataURL method is used to read the contents of the specified Blob or File.
-      //  When the read operation is finished, the readyState becomes DONE, and the loadend is
-      // triggered. At that time, the result attribute contains the data as a data: URL representing
-      // the file's data as a base64 encoded string.
-      // Source: https://developer.mozilla.org/en-US/docs/Web/API/FileReader/readAsDataURL
-
-      reader.onLoadEnd.listen(
-        // After file finiesh reading and loading, it will be uploaded to firebase storage
-        (loadEndEvent) async {
-          onSelected(file);
-        },
-      );
-    },
-  );
 }
 
 Stream<CurrentUser> userStream() {
@@ -155,4 +89,48 @@ Stream<CurrentUser> userStream() {
       .doc('JtcAlXoD8DVWduTeqS2F')
       .snapshots()
       .map((doc) => CurrentUser.fromDoc(doc));
+}
+
+Future<Uri> downloadUrl(String photoUrl) {
+  return fb
+      .storage()
+      .refFromURL('gs://happy-haris-play.appspot.com/')
+      .child(photoUrl)
+      .getDownloadURL();
+}
+
+void uploadImage({@required Function(File file) onSelected}) {
+  InputElement uploadInput = FileUploadInputElement()..accept = 'image/*';
+  uploadInput.click();
+
+  uploadInput.onChange.listen((event) {
+    final file = uploadInput.files.first;
+    final reader = FileReader();
+    reader.readAsDataUrl(file);
+    reader.onLoadEnd.listen((event) {
+      onSelected(file);
+    });
+  });
+}
+
+void uploadToStorage(CurrentUser user) {
+  final dateTime = DateTime.now();
+  final userId = user.id;
+  final path = '$userId/$dateTime';
+  uploadImage(
+    onSelected: (file) {
+      fb
+          .storage()
+          .refFromURL('gs://happy-haris-play.appspot.com/')
+          .child(path)
+          .put(file)
+          .future
+          .then((_) {
+        FirebaseFirestore.instance
+            .collection('users')
+            .doc(user.id)
+            .update({'photo_url': path});
+      });
+    },
+  );
 }
